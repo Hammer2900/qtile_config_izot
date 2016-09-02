e9a223b0d953376e9bdabd6b113742792366d74a
Izot <finalka@gmail.com>
2016-09-02 21:14:47 +0300

    group windows

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from libqtile.config import Key, Screen, Group, Drag, Click
 from libqtile.command import lazy
-from libqtile import layout, bar, widget
+from libqtile import layout, bar, widget, hook
 
 mod = "mod4"
 
@@ -122,6 +122,59 @@ screens = [
         ),
     ),
 ]
+
+group_app_dict = {
+    'firefox-aurora': 'browser',
+    'skype': 'skype'
+}
+
+@hook.subscribe.client_new
+def dialogs(window):
+    if (window.window.get_wm_type() == 'dialog' or window.window.get_wm_transient_for()):
+        window.floating = True
+
+    @hook.subscribe.client_new
+    def grouper(window, windows=group_app_dict):
+
+        """
+        This function relies on the contentious feature of default arguments
+        where upon function definition if the argument is a mutable datatype,
+        then you are able to mutate the data held within that object.
+        Current usage:
+        {window_name: group_name}
+        or for grouping windows to different groups you will need to have a
+        list under the window-key with the order that you're starting the
+        apps in.
+        See the 'runner()' function for an example of using this method.
+        Here:
+        {window_name: [group_name1, group_name2]}
+        Window name can be found via window.window.get_wm_class()
+        """
+
+        windowtype = window.window.get_wm_class()[0]
+
+        # if the window is in our map
+        if windowtype in windows.keys():
+
+            # opening terminal applications gives
+            # the window title the same name, this
+            # means that we need to treat these apps
+            # differently
+
+            if windowtype != 'urxvt':
+                window.togroup(windows[windowtype])
+                windows.pop(windowtype)
+
+            # if it's not on our special list,
+            # we send it to the group and pop
+            # that entry out the map
+            else:
+                try:
+                    window.togroup(windows[windowtype][0])
+                    windows[windowtype].pop(0)
+                except IndexError:
+                    pass
+
 
 # Drag floating layouts.
 mouse = [
