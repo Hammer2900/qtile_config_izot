bd3f6549cebd09fb7312af6e660e8246611f4f31
Izot <finalka@gmail.com>
2016-09-17 19:59:40 +0300

    add subl test to group

--- config.py
+++ config.py
@@ -182,53 +182,59 @@ group_app_dict = {
     'skype': 'skype'
 }
 
+
+@hook.subscribe.client_new
+def vue_tools(window):
+    if window.window.get_wm_class() == ('sublime_text', 'Sublime_text'):
+        window.togroup('text')
+
 @hook.subscribe.client_new
 def dialogs(window):
     floating_types = ['notification', 'toolbar', 'splash', 'dialog']
     if window.window.get_wm_type() in floating_types or window.window.get_wm_transient_for():
         window.floating = True
 
-    @hook.subscribe.client_new
-    def grouper(window, windows={'firefox-aurora': 'browser', 'skype': 'skype', 'pcmanfm': 'fm', 'jetbrains-pycharm': 'pycharm'}):
+@hook.subscribe.client_new
+def grouper(window, windows={'firefox-aurora': 'browser', 'skype': 'skype', 'pcmanfm': 'fm', 'jetbrains-pycharm': 'pycharm'}):
 
-        """
-        This function relies on the contentious feature of default arguments
-        where upon function definition if the argument is a mutable datatype,
-        then you are able to mutate the data held within that object.
-        Current usage:
-        {window_name: group_name}
-        or for grouping windows to different groups you will need to have a
-        list under the window-key with the order that you're starting the
-        apps in.
-        See the 'runner()' function for an example of using this method.
-        Here:
-        {window_name: [group_name1, group_name2]}
-        Window name can be found via window.window.get_wm_class()
-        """
+    """
+    This function relies on the contentious feature of default arguments
+    where upon function definition if the argument is a mutable datatype,
+    then you are able to mutate the data held within that object.
+    Current usage:
+    {window_name: group_name}
+    or for grouping windows to different groups you will need to have a
+    list under the window-key with the order that you're starting the
+    apps in.
+    See the 'runner()' function for an example of using this method.
+    Here:
+    {window_name: [group_name1, group_name2]}
+    Window name can be found via window.window.get_wm_class()
+    """
 
-        windowtype = window.window.get_wm_class()[0]
+    windowtype = window.window.get_wm_class()[0]
 
-        # if the window is in our map
-        if windowtype in windows.keys():
+    # if the window is in our map
+    if windowtype in windows.keys():
 
-            # opening terminal applications gives
-            # the window title the same name, this
-            # means that we need to treat these apps
-            # differently
+        # opening terminal applications gives
+        # the window title the same name, this
+        # means that we need to treat these apps
+        # differently
 
-            if windowtype != 'urxvt':
-                window.togroup(windows[windowtype])
-                windows.pop(windowtype)
+        if windowtype != 'urxvt':
+            window.togroup(windows[windowtype])
+            windows.pop(windowtype)
 
-            # if it's not on our special list,
-            # we send it to the group and pop
-            # that entry out the map
-            else:
-                try:
-                    window.togroup(windows[windowtype][0])
-                    windows[windowtype].pop(0)
-                except IndexError:
-                    pass
+        # if it's not on our special list,
+        # we send it to the group and pop
+        # that entry out the map
+        else:
+            try:
+                window.togroup(windows[windowtype][0])
+                windows[windowtype].pop(0)
+            except IndexError:
+                pass
 
 
 # Drag floating layouts.
